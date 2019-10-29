7242313bb1b26249b57bf9ce0cc7f7eec321977b
Rolando Concarella <r.concarella@gmail.com>
2019-10-29 13:12:12 +0100

    import java.util.Map

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -83,6 +83,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 @SuppressLint("SetJavaScriptEnabled")
